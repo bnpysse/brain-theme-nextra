9be8ddb7c7ebf3dd763fbe56ac76d8229d512527
bnpysse <bnpysse@aliyun.com>
2026-08-13 16:48:06 +0800

    Initial Nextra setup

---
+++ theme.config.jsx
@@ -0,0 +1,22 @@
+export default {
+  logo: <span>Woodman's Second Brain</span>,
+  project: {
+    link: 'https://github.com/bnpysse/obsidian-brain-data',
+  },
+  docsRepositoryBase: 'https://github.com/bnpysse/obsidian-brain-data',
+  footer: {
+    text: 'Woodman 第二大脑 © 2026',
+  },
+  search: {
+    placeholder: '搜索知识库...',
+  },
+  toc: {
+    title: '本页目录',
+  },
+  editLink: {
+    text: '在 GitHub 上编辑此页',
+  },
+  feedback: {
+    content: '有疑问？提供反馈 →',
+  },
+}
